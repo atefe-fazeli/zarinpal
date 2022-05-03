75e82b85427f32723ff227923f8a50dfe4cd9574
atefe fazeli <atefefazeli1996@gmail.com>
2022-05-03 21:42:12 +0430

    navbar html finished

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <div></div>
+            <div>
+                <ul>
+                    <li>تعرفه ها </li>
+                    <li>اپلیکیشن</li>
+                    <li>سوالات متداول</li>
+                    <li>محصولات</li>
+                    <li>تماس با ما</li>
+                </ul>
+            </div>
+            <div><button>زرین پال من</button></div>
+            
+        </div>
+    );
+};
+
+export default Navbar;

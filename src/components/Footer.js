beb3fa1448238c7fe7ac880082c39fbb92b3bcf7
atefe fazeli <atefefazeli1996@gmail.com>
2022-05-04 16:49:04 +0430

    footer module component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,63 @@
 import React from 'react';
+import {Link} from "react-router-dom"
+import styles from "./Footer.module.css"
+
 
 const Footer = () => {
     return (
-        <div>
+        <div className={styles.footermaincontainer}>
+            <div className={styles.firstdivcontainer}>
+                <h1>به خانواده‌ی یک میلیونی زرین‌پال بپیوندید.</h1>
+                <h3>یک ماه رایگان، هدیه‌ی عضویت زرین‌پال به شما</h3>
+                <button >شروع کنید</button>
+            </div>
+            <div className={styles.seconddivcontainer}>
+                <h3>پشتیبانی ۲۴ ساعته، ۷ روز هفته| شماره تماس:   <span className={styles.footerspan}> ۴۱۲۳۹-۰۲۱</span></h3>
+                <Link to="/">درخواست پشتیبانی</Link>
+             </div>
+            <div className={styles.thireddivcontainer}>
+                <div >
+                    <h2>محصولات</h2>
+                    <Link to="/" className={styles.footerlink}>زرین لینک </Link>
+                    <Link to="/" className={styles.footerlink}>درگاه پرداخت اینترنتی</Link>
+                    <Link to="/" className={styles.footerlink}>زرین کارت</Link>
+                    <Link to="/" className={styles.footerlink}>تسهیم</Link>
+                </div>
+
+                <div>
+                <h2>آشنایی با زرین پال</h2>
+                    <Link to="/" className={styles.footerlink}>تعرفه ها </Link>
+                    <Link to="/" className={styles.footerlink}>در باره ما </Link>
+                    <Link to="/" className={styles.footerlink}>سوالات متداول</Link>
+                    <Link to="/" className={styles.footerlink}>همکاری در فروش</Link>
+                </div>
+
+                <div>
+                <h2>ارتباط بیشتر </h2>
+                    <Link to="/" className={styles.footerlink}>تماس با ما</Link>
+                    <Link to="/" className={styles.footerlink}>قوانین و مقررات</Link>
+                    <Link to="/" className={styles.footerlink}>حریم خصوصی</Link>
+                </div>
+
+                <div>
+                <h2>منابع</h2>
+                    <Link to="/" className={styles.footerlink}>دریافت شماره شبا</Link>
+                    <Link to="/" className={styles.footerlink}>زرین بین</Link>
+                    <Link to="/" className={styles.footerlink}>توسعه دهندگان</Link>
+                    <Link to="/" className={styles.footerlink}>وبلاگ</Link>
+                </div>
+
+                <div className={styles.socialdivcontainer}>
+                <h2>زرین پال در شبکه های اجتماعی:</h2>
+                    <Link to="/"></Link>
+                    <Link to="/"></Link>
+                    <Link to="/"></Link>
+                    <Link to="/"></Link>
+                </div>
+
+            </div>
+            <div>زرین پال 1400-1398</div>
+
             
         </div>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,15 +4,15 @@ import styles from "./Navbar.module.css"
 const Navbar = () => {
     return (
         <div className={styles.navbarcontainer}>
-            <div className={styles.logocontainer}>لوگو</div>
+            <div className={styles.logocontainer}></div>
             <div className={styles.buttonandlistcontainer}>
             <div className={styles.mainlistcontainer}>
                 <ul className={styles.listcontainer}>
-                    <li>تعرفه ها </li>
-                    <li>اپلیکیشن</li>
-                    <li>سوالات متداول</li>
-                    <li>محصولات</li>
+                    <li>محصولات </li>
+                    <li>تعرفه ها</li>
+                    <li>توسعه دهندگان</li>
                     <li>تماس با ما</li>
+                    <li>بیشتر</li>
                 </ul>
             </div>
             <button>زرین پال من</button>

0c4168853254fd3074d7262cbb100235643921f6
atefe fazeli <atefefazeli1996@gmail.com>
2022-05-05 11:59:56 +0430

    section one module component created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Baner from './components/Baner';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import SectionOne from './components/SectionOne';
 
 function App() {
   return (
     <div className="App">
      <Navbar />
      <Baner/>
+     <SectionOne />
      <Footer />
      
     </div>
--- src/components/SectionOne.js
+++ src/components/SectionOne.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import styles from './SectionOne.module.css'
 
 const SectionOne = () => {
     return (
         <div className={styles.SecOneMainContainer}>
             <div>
-                 <h3></h3>
-                 <h1></h1>
-                 <p></p>
-                 <button></button>
-                 <button></button>
+                 <h3>درگاه پرداخت اینترنتی</h3>
+                 <h1>مهندسی شده برای رشد و فروش بیش‌تر</h1>
+                 <p>درگاه پرداخت زرین‌پال، با اتصالِ همزمان به درگاه‌های متنوع و معتبر بانکی(PSPها)،کاربران را به سریع‌ترین و مطمئن‌ترین درگاه بانکی منتقل می‌کند و به واسطه‌ی قابلیت مسیردهی هوشمند، باعث افزایش درصد تراکنش‌های موفق می‌شود</p>
+                 <button>ساخت درگاه پرداخت</button>
+                 <button>بیشتر بدانید</button>
             </div>
             <div className={styles.SecOneImgContainer}>
 

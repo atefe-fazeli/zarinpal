11c155b04d5f7c9358e360ddf1e2c4eafe6aa9e8
atefe fazeli <atefefazeli1996@gmail.com>
2022-05-05 17:34:28 +0430

    some icon aded to section six  component

--- src/components/SectionSix.js
+++ src/components/SectionSix.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import styles from './SectionSix.module.css'
+import { BiSupport } from 'react-icons/bi';
+import { MdOutlineVerified } from 'react-icons/md';
+import { MdOutlineCallSplit } from 'react-icons/md';
+
+
 const SectionSix = () => {
     return (
         <div className={styles.SecSixmaincontainer}>
@@ -9,19 +14,19 @@ const SectionSix = () => {
             </div>
             <div>
                 <div>
-                    <h1>ضمانت پرداخت</h1>
+                    <h1>ضمانت پرداخت<span><MdOutlineVerified /></span></h1>
                     <p>درگاه پرداخت زرین‌پال، تجربه‌ی پرداخت اینترنتی آسان، سریع و امن را به مشتریان کسب و کارهای آنلاین هدیه می‌دهد.</p>
                 </div>
                 <div>
-                    <h1>مسیردهی هوشمند</h1>
+                    <h1>مسیردهی هوشمند<span><MdOutlineCallSplit /></span></h1>
                     <p>استانداردهای متفاوت زرین‌پال در بازه‌های زمانی مختلف، باعث می‌شود کاربران به بهترین درگاه پرداخت با بالاترین میزان تراکنش‌های موفق هدایت شوند.</p>
                 </div>
                 <div>
-                    <h1>پشتیبانی ۲۴/۷</h1>
+                    <h1>پشتیبانی ۲۴/۷<span><BiSupport /></span></h1>
                     <p>هفت روزِ هفته، با پشتیبانی ۲۴ ساعته‌ی زرین‌پال، آماده‌ی پاسخ‌گویی و راهنمایی به کاربران هستیم.</p>
                 </div>
                 <div>
-                    <h1>امنیتِ پرداخت</h1>
+                    <h1>امنیتِ پرداخت<span><BiSupport /></span></h1>
                     <p>درگاه امن زرین‌پال، ضمانتی برای امنیت اطلاعات بانکی افراد، هنگام پرداخت‌‌های اینترنتی آن‌هاست.</p>
                 </div>
             </div> 

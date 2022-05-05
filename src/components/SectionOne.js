3199d62b763c1c8c6200cdc6de7c69f991ba448a
atefe fazeli <atefefazeli1996@gmail.com>
2022-05-05 12:35:38 +0430

    SectionTwo component created

--- src/components/SectionOne.js
+++ src/components/SectionOne.js
@@ -4,15 +4,15 @@ import styles from './SectionOne.module.css'
 const SectionOne = () => {
     return (
         <div className={styles.SecOneMainContainer}>
+            
+            <div className={styles.SecOneImgContainer}></div>
+
             <div>
                  <h3>درگاه پرداخت اینترنتی</h3>
                  <h1>مهندسی شده برای رشد و فروش بیش‌تر</h1>
                  <p>درگاه پرداخت زرین‌پال، با اتصالِ همزمان به درگاه‌های متنوع و معتبر بانکی(PSPها)،کاربران را به سریع‌ترین و مطمئن‌ترین درگاه بانکی منتقل می‌کند و به واسطه‌ی قابلیت مسیردهی هوشمند، باعث افزایش درصد تراکنش‌های موفق می‌شود</p>
                  <button>ساخت درگاه پرداخت</button>
                  <button>بیشتر بدانید</button>
-            </div>
-            <div className={styles.SecOneImgContainer}>
-
             </div>
 
 

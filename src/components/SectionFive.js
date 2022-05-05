7784d1f028998015854487f939b1a68de7c37227
atefe fazeli <atefefazeli1996@gmail.com>
2022-05-05 14:48:21 +0430

    section five  component html tags completed

---
+++ src/components/SectionFive.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './SectionFive.module.css'
+
+const SectionFive = () => {
+    return (
+        <div className={styles.SecFiveMainContainer}>
+
+            <div className={styles.SecFiveImgContainer}></div>
+
+            <div>
+                 <h3>درگاه پرداخت اینترنتی</h3>
+                 <h1>مهندسی شده برای رشد و فروش بیش‌تر</h1>
+                 <p>درگاه پرداخت زرین‌پال، با اتصالِ همزمان به درگاه‌های متنوع و معتبر بانکی(PSPها)،کاربران را به سریع‌ترین و مطمئن‌ترین درگاه بانکی منتقل می‌کند و به واسطه‌ی قابلیت مسیردهی هوشمند، باعث افزایش درصد تراکنش‌های موفق می‌شود</p>
+                 <button>ساخت درگاه پرداخت</button>
+                 <button>بیش تر بدانید</button>
+            </div>
+
+
+        </div>
+       
+    );
+};
+
+export default SectionFive;
+

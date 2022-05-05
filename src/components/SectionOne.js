dd6b15b10476ab4b10941a1334b1f9ef4e9311fd
atefe fazeli <atefefazeli1996@gmail.com>
2022-05-05 11:53:14 +0430

    section one html tags completed

---
+++ src/components/SectionOne.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const SectionOne = () => {
+    return (
+        <div className={styles.SecOneMainContainer}>
+            <div>
+                 <h3></h3>
+                 <h1></h1>
+                 <p></p>
+                 <button></button>
+                 <button></button>
+            </div>
+            <div className={styles.SecOneImgContainer}>
+
+            </div>
+
+
+        </div>
+       
+    );
+};
+
+export default SectionOne;
+

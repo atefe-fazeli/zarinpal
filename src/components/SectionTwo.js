cd4d3a38925a91e2eb0afc0f8f1c062161e398be
atefe fazeli <atefefazeli1996@gmail.com>
2022-05-05 13:12:48 +0430

    section two  html tags completed

---
+++ src/components/SectionTwo.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const SectionTwo = () => {
+    return (
+        <div className={styles.SecTwoContainer}>
+            
+
+            <div>
+                 <h3>زرین‌لینک</h3>
+                 <h1>پرداخت در شبکه‌های اجتماعی</h1>
+                 <p>زرین‌لینک با مدیریت فرآیند فروش به روشی هوشمندانه و سریع، باعث افزایش فروش محصولات و خدمات، از طریق شبکه‌های اجتماعی مانند اینستاگرام، تلگرام یا وب‌سایت می‌شود</p>
+                 <button>ساخت درگاه پرداخت</button>
+                 <button>بیشتر بدانید</button>
+            </div>
+            <div className={styles.SecTwoImgContainer}></div>
+
+
+
+        </div>
+            
+    
+    );
+};
+
+export default SectionTwo;

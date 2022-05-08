dd69e056f745983916e5f3fc517c3c9a068e81d5
atefe fazeli <atefefazeli1996@gmail.com>
2022-05-08 19:52:17 +0430

    navbar divs created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,36 @@
 import React, { useState } from 'react';
-import styles from "./Navbar.module.css"
-import Onmousefirstcomponent from './Onmousefirstcomponent';
-import Onmousesecondcomponent from './Onmousesecondcomponent';
-
+import styles from "./Navbar.module.css";
+import {Link} from 'react-router-dom';
 const Navbar = () => {
-    const [data,setData]=useState(false)
     return (
         <div className={styles.navbarcontainer}>
             <div className={styles.logocontainer}></div>
             <div className={styles.buttonandlistcontainer}>
             <div className={styles.mainlistcontainer}>
                 <ul className={styles.listcontainer}>
-                   <li><button onClick={()=>setData(!data)} className={data ? <Onmousefirstcomponent /> : <Onmousesecondcomponent/>}>محصولات </button></li>
-                   {data ?<div> <Onmousefirstcomponent /> </div>:  <div><Onmousesecondcomponent/></div>}
+                   <li className={styles.productsdiv}> محصولات </li>
+                     <div className={styles.productscontainr}>
+                     <ul>
+                        <li><Link to="/"><span>درگاه پرداخت</span>مهندسی شده برای فروش بیش تر</Link></li>
+                        <li><Link to="/"><span>زرین لینک</span>لینک پرداخت در شبکه های اجتماعی </Link></li>
+                        <li><Link to="/"><span>زرین پلاس</span>ابزار هوشمند سوددهی</Link></li>
+                        <li><Link to="/"><span>زرین کارت</span>طلایی ترین کارت بانکی</Link></li>
+                        <li><Link to="/"><span>تسهیم</span>درگاه پرداخت اشتراکی</Link></li>
+                      </ul>
+                     </div>
                     <li>تعرفه ها</li>
                     <li>توسعه دهندگان</li>
                     <li>تماس با ما</li>
-                    <li>بیشتر</li>
+                    <li className={styles.detailsdiv}>بیشتر</li>
+                    <div className={styles.detailsdivcontainer}>
+                      <ul>
+                        <li><Link to="/">وبلاگ</Link></li>
+                        <li><Link to="/">دریافت شماره شبا</Link></li>
+                        <li><Link to="/">سوالات متداول</Link></li>
+                        <li><Link to="/">اپلیکیشن</Link></li>
+                        <li><Link to="/">زرین بین </Link></li>
+                      </ul>
+                    </div>
                 </ul>
             </div>
             <button>زرین پال من</button>
